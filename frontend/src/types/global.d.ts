36714f302ad0f9f761c9dccf99c3bccc7df0dc7b
Yura Piontkovskyi <yurapion@gmail.com>
2025-06-27 23:26:43 +0200

    robot plus smart contract

---
+++ frontend/src/types/global.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  interface Window {
+    ethereum?: {
+      request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+      on: (event: string, callback: (...args: unknown[]) => void) => void;
+      removeListener: (event: string, callback: (...args: unknown[]) => void) => void;
+      isMetaMask?: boolean;
+    };
+  }
+}
+
+export {}; 
